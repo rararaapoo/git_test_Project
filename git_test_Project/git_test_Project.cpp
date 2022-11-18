7a9d013cca3d628e5d4f656203fae824fdf5881e
madonguk1 <73094562+rararaapoo@users.noreply.github.com>
2022-11-18 09:37:03 +0900

    リモートリポジトリでコンフリクト変更

--- git_test_Project/git_test_Project.cpp
+++ git_test_Project/git_test_Project.cpp
@@ -6,7 +6,7 @@
 int main()
 {
     std::cout << "Hello World!\n";
-    std::cout << "GitHub Clone changed\n";
+    std::cout << "GitHub Clone Changed\n";
 }
 
 // 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
